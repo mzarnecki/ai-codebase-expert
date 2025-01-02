98c9381e8c240de028f540877c3571d2da7cb39d
Michał Żarnecki <michal.zarnecki@companyhouse.de>
2025-01-02 16:01:02 +0100

    improve app

--- app/llm/LLMChainProvider.py
+++ app/llm/LLMChainProvider.py
@@ -2,12 +2,12 @@ import streamlit as st
 from langchain.agents.chat.base import ChatAgent
 from langchain.chains.conversation.base import ConversationChain
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.chains.router import MultiRetrievalQAChain
 from langchain.memory import ConversationBufferMemory
+from langchain.retrievers import MergerRetriever
 from langchain_community.chat_models import ChatOpenAI
 from langchain_community.tools import DuckDuckGoSearchResults
-from langchain_community.utilities.dataforseo_api_search import DataForSeoAPIWrapper
 from langchain_core.prompts import ChatPromptTemplate
-from transformers import Tool
 
 from app.db.EnumDocsCollection import EnumDocsCollection
 from app.db.VectorStore import VectorStore
@@ -20,7 +20,7 @@ from app.streaming import StreamHandler
 
 class LLMChainProvider:
     @st.spinner('Analyzing documents..')
-    def getLLMPrparationChainResult(llm: ChatOpenAI, ticket: str):
+    def getLLMPrparationChainResult(self, llm: ChatOpenAI, ticket: str):
         # Setup memory for contextual conversation
         memory = ConversationBufferMemory()
 
@@ -30,34 +30,54 @@ class LLMChainProvider:
             memory=memory,
             verbose=False
         )
+
         result = llm_chain.invoke(input={"input": """
             You are a chatbot tasked with solving software project issues.
             The project is a website companyhouse.de and it's written in PHP language using Yii2 framework.
-            Prepare message that will be used for sematic search in database for project code and project documentation.
-            Prepare message based on issue description below. Say which files should be checked.
-            """ + ticket},
+            Prepare message that will be used for sematic search in database for project code, project documentation and framework documentation.
+            Prepare list of information and concepts that are relevant to answering for the problem described below. Take also into consideration directory structure of the project
+            """ + ticket + "\n\n" + self.getProjectDirStructure()},
         )
 
         return result
 
-    @st.spinner('Analyzing documents..')
-    def getLLMConversationalChainResult(llm: ChatOpenAI, ticket: str)->ConversationalRetrievalChain:
-        vectordb = VectorStore.get_vector_store(EnumDocsCollection.COMPANYHOUSE_PROJ_CODE.value)
+    def getProjectDirStructure(self):
+        with open("data/baseInformation/projectDirectoryStructure.txt", "r") as f:
+            dir_structure = f.readlines()
+        return "\n".join(dir_structure)
 
-        # Define retriever
-        retriever = vectordb.as_retriever(
+    @st.spinner('Analyzing documents..')
+    def getLLMConversationalChainResult(self, llm: ChatOpenAI, ticket: str)->ConversationalRetrievalChain:
+        # Define retrievers
+        vectordbcode = VectorStore.get_vector_store(EnumDocsCollection.COMPANYHOUSE_PROJ_CODE.value)
+        retrievercode = vectordbcode.as_retriever(
             search_type='similarity',
-            search_kwargs={'k':10, 'fetch_k':10}
+            search_kwargs={'k':10}
         )
+
+        vectordbprojdocs = VectorStore.get_vector_store(EnumDocsCollection.COMPANYHOUSE_PROJ_DOCS.value)
+        retrieverprojdocs = vectordbprojdocs.as_retriever(
+            search_type='similarity',
+            search_kwargs={'k':5}
+        )
+
+        vectordbframework = VectorStore.get_vector_store(EnumDocsCollection.COMPANYHOUSE_PROJ_CODE.value)
+        retrieverframework = vectordbframework.as_retriever(
+            search_type='similarity',
+            search_kwargs={'k':5}
+        )
+
+        retriever = MergerRetriever(retrievers = [retrievercode, retrieverframework, retrieverprojdocs])
 
         # Setup memory for contextual conversation
         memory = ConversationBufferMemory(
             memory_key='chat_history',
+            input_key='question',
             output_key='answer',
             return_messages=True
         )
 
-        system_message_prompt = PromptTemplateProvider.getPromptTemplate(ticket)
+        system_message_prompt = PromptTemplateProvider.getPromptTemplate(ticket, self.getProjectDirStructure())
         prompt = ChatPromptTemplate.from_messages([system_message_prompt])
 
         # Setup LLM and QA chain
@@ -66,18 +86,18 @@ class LLMChainProvider:
             retriever=retriever,
             memory=memory,
             return_source_documents=True,
-            verbose=False,
+            verbose=True,
             combine_docs_chain_kwargs={"prompt": prompt}
         )
         st_cb = StreamHandler(st.empty())
         result = llm_chain.invoke(
-            {"question": "Find solution for described issue."},
+            {"question": "Find solution for described issue.", "closure": "", "main": ""},
             {"callbacks": [st_cb]}
         )
 
         return result
 
-    def getLLMAgent(llm: ChatOpenAI, ticket: str):
+    def getLLMAgent(self, llm: ChatOpenAI, ticket: str):
 
         tools = [DuckDuckGoSearchResults()]
         agent = ChatAgent.from_llm_and_tools(llm, tools, verbose=True)
@@ -94,6 +114,6 @@ class LLMChainProvider:
             
             For solution use information from project code and documentation below.
             
-            """
+            """ + self.getProjectDirStructure()
 
         return executor.run(input= input, callbacks=[st_cb])
--- index.py
+++ index.py
@@ -25,12 +25,13 @@ class CustomDocChatbot:
 
             utils.display_msg(ticket.__str__(), 'user')
 
+            provider = LLMChainProvider()
             with st.chat_message("assistant"):
-                resultRag = LLMChainProvider.getLLMPrparationChainResult(self.llm, ticket.__str__())
+                resultRag = provider.getLLMPrparationChainResult(self.llm, ticket.__str__())
                 print(resultRag['response'])
                 print("\n")
-                result = LLMChainProvider.getLLMConversationalChainResult(self.llm,  ticket.__str__() + resultRag['response'])
-                # result = LLMChainProvider.getLLMAgent(self.llm, ticket.__str__())
+                result = provider.getLLMConversationalChainResult(self.llm,  ticket.__str__() + resultRag['response'])
+                # result = provider.getLLMAgent(self.llm, ticket.__str__())
                 response = result["answer"]
                 st.session_state.messages.append({"role": "assistant", "content": response})
                 utils.print_qa(CustomDocChatbot, ticket.__str__(), response)
